3bea3e37907285e6e9ce078884ff1f2356640f9c
Rajnish379 <rajinish77@gmail.com>
2024-07-03 10:32:22 -0500

    Changed the text of PC again

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-      I've made a couple of games in that language. I also like Java and Python. Anyway regardless of the language, I just like programming.
+    pc: `This is my PC. I work mostly in Java and Python these days.
+      I've made a lot of projects and did lot of practice on these. I also like Javascript and React. Anyway regardless of the language, I just like programming.
       Here is my <a href="https://github.com/Rajnish379" target="_blank">Github</a>!`,
     "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
     "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
